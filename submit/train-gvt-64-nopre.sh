c91bdf15e4629ce11dc895664b0ff52b78d879a2
ruiyiw <ruiyi-pamela-wang@gmail.com>
2021-10-19 17:57:06 -0400

    New submit

--- submit/train-gvt-64-nopre.sh
+++ submit/train-gvt-64-nopre.sh
@@ -2,4 +2,4 @@ cd /share03/rwang/wry/vae-trans || exit
 source env/bin/activate
 cd VT-summ
 
-python3 main.py --dataset gigaword --model cvaetrs --emb_dim 300 --hidden_dim 300 --hop 4 --heads 4 --cuda --batch_size 64 --lr 0.001 --pretrain_emb --kl_ceiling 0.08 --aux_ceiling 1 --full_kl_step 15000 --save_path gvt-64-nopre/cvae_trs/ > gvt-64-nopre/cvae_trs/out.txt
+python3 main.py --dataset gigaword --model cvaetrs --emb_dim 300 --hidden_dim 300 --hop 4 --heads 4 --cuda --batch_size 64 --lr 0.001 --pretrain_emb --kl_ceiling 0.08 --aux_ceiling 1 --full_kl_step 15000 --save_path tmp/gvt-64-nopre-test/cvae_trs/ > tmp/gvt-64-nopre-test/cvae_trs/out.txt
