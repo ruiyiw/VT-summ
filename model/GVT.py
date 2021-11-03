d987d99d4660c393e6d946c700b5f475321c09da
ruiyiw <ruiyi-pamela-wang@gmail.com>
2021-11-02 20:25:41 -0400

    Find inplace operation

--- model/GVT.py
+++ model/GVT.py
@@ -486,7 +486,7 @@ class CvaeTrans(nn.Module):
                 # Add gate
                 gate_c = self.gate(input_vector[:,0].clone().detach())
                 gate_c = torch.sigmoid(gate_c)
-                input_vector[:,0] = gate_c * input_vector[:,0] + (gate_c.new_tensor([1]) - gate_c) * z
+                input_vector[:,0] = gate_c * (input_vector[:,0].clone().detach()) + (gate_c.new_tensor([1]) - gate_c) * z
             else:
                 # input_vector[:,0] = input_vector[:,0]+meta
                 input_vector[:,0] = input_vector[:,0]
