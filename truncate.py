14d4901c7a99d3a84dd0f2a18ce9f9c2355a5333
ruiyiw <ruiyi-pamela-wang@gmail.com>
2021-11-12 17:32:17 -0500

    Truncate cnn_dm

---
+++ truncate.py
@@ -0,0 +1,38 @@
+# from utils.data_loader import prepare_data_seq
+# from utils import config
+# from model.seq2seq import SeqToSeq
+# if config.v2:
+#     from model.SVT import CvaeTrans
+# else:
+#     from model.GVT import CvaeTrans
+# from model.common_layer import evaluate,evaluate_tra, count_parameters, make_infinite, get_kld
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# from torch.nn.init import xavier_uniform_
+# from copy import deepcopy
+# from tqdm import tqdm
+# import os
+# import time 
+# import numpy as np
+# import math
+# from tensorboardX import SummaryWriter
+# import logging
+
+# # data_loader_tra, data_loader_val, data_loader_tst, vocab, program_number = prepare_data_seq(batch_size=config.batch_size)
+# data_loader_tra, data_loader_val, data_loader_tst, vocab = prepare_data_seq(batch_size=config.batch_size) # data_loader with batch
+# logging.info("dataloader size - tra: {} dev: {} tst: {}".format(data_loader_tra.__len__(), data_loader_val.__len__(), data_loader_tst.__len__()))
+
+from nltk.tokenize import sent_tokenize
+path = "data/cnn_dm_data/"
+text = []
+with open(path+"train.target", 'r', encoding='utf-8') as fin:
+    for line in fin:
+        line = line.strip().split()
+        token = sent_tokenize(line)
+        text.append(token[0]+token[1])
+
+with open(path+"train.target.pre", 'w', encoding='utf-8') as fout:
+    for t in text:
+        fout.write(t)
+        fout.write('\n')
