0a8e0efd984280e498cc81e23d52d09e7d727194
ruiyiw <ruiyi-pamela-wang@gmail.com>
2021-11-13 17:10:54 -0500

    back to original

--- model/GVT.py
+++ model/GVT.py
@@ -117,7 +117,7 @@ class Decoder(nn.Module):
     Refer Fig.1 in https://arxiv.org/pdf/1706.03762.pdf
     """
     def __init__(self, embedding_size, hidden_size, num_layers, num_heads, total_key_depth, total_value_depth,
-                 filter_size, max_length=300, input_dropout=0, layer_dropout=0, 
+                 filter_size, max_length=200, input_dropout=0, layer_dropout=0, 
                  attention_dropout=0.1, relu_dropout=0.1, universal=False):
         """
         Parameters:
