510fafe41f9b3dcd5bc6878eaa13ca675ac904b7
ruiyiw <ruiyi-pamela-wang@gmail.com>
2021-11-12 20:22:23 -0500

    Update cnn_dm

--- model/GVT.py
+++ model/GVT.py
@@ -27,7 +27,7 @@ class Encoder(nn.Module):
     Refer Fig.1 in https://arxiv.org/pdf/1706.03762.pdf
     """
     def __init__(self, embedding_size, hidden_size, num_layers, num_heads, total_key_depth, total_value_depth,
-                 filter_size, max_length=config.enc_maxlen, input_dropout=0, layer_dropout=0, 
+                 filter_size, max_length=1000, input_dropout=0, layer_dropout=0, 
                  attention_dropout=0.1, relu_dropout=0.1, use_mask=False, universal=False):
         """
         Parameters:
