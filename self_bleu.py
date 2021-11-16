f480203a2b402f4fa4cdf915b46cdaae763a6197
ruiyiw <ruiyi-pamela-wang@gmail.com>
2021-11-15 20:07:01 -0500

    test

--- self_bleu.py
+++ self_bleu.py
@@ -25,7 +25,7 @@ with open(ref, 'r', encoding='utf-8') as rfile:
         word = word_tokenize(line)
         r.append(word)
 
-weights = {'unigram': (1), 'bigram': (1/2., 1/2.), 'trigram': (1/3., 1/3., 1/3.)}
+weights = {'bigram': (1/2., 1/2.), 'trigram': (1/3., 1/3., 1/3.)}
 
 bleu = BLEU(r, weights)
 score_b = bleu.get_score(h)
@@ -33,5 +33,5 @@ score_b = bleu.get_score(h)
 self_bleu = SelfBLEU(h, weights)
 score_s = self_bleu.get_score()
 
-print("D-1: {} D-2: {} D-3: {}".format(sum(score_b['unigram'])/len(h), sum(score_b['bigram'])/len(h), sum(score_b['trigram'])/len(h)))
-print("S-1: {} S-2: {} S-3: {}".format(sum(score_s['unigram'])/len(h), sum(score_s['bigram'])/len(h), sum(score_s['trigram'])/len(h)))
+print("D-1: {} D-2: {} D-3: {}".format(sum(score_b['bigram'])/len(h), sum(score_b['trigram'])/len(h)))
+print("S-1: {} S-2: {} S-3: {}".format(sum(score_s['bigram'])/len(h), sum(score_s['trigram'])/len(h)))
